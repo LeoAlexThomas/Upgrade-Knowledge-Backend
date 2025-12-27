1576e446e1d75a899811c81e544663d344069eba
LeoAlexThomas <leoalex960@gmail.com>
2025-12-27 11:36:51 +0530

    added tutor slot control for slot

--- Controllers/SlotController.js
+++ Controllers/SlotController.js
@@ -14,3 +14,25 @@ export const addSlot = async (slot, tutorId) => {
   await newSlot.save();
   return newSlot;
 };
+
+export const getSlotsFromTutor = async (req, res) => {
+  try {
+    const tutorId = req.params.tutorId;
+
+    if (!tutorId) {
+      res.status(400).json({
+        message: "Tutor id is not found",
+      });
+    }
+
+    const slots = await Slot.find({ tutor: tutorId });
+
+    res.status(200).json({
+      data: slots,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Unable to get slots, Please try again later",
+    });
+  }
+};
